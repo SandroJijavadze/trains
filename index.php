46433f3fb13516127aad5aa050f48c3198d81a94
Sandro <s.jijavadze@hotmail.com>
2018-08-02 21:26:29 +0400

    getResponse function and 7 day loop

--- index.php
+++ index.php
@@ -4,31 +4,38 @@ require_once __DIR__ .'/vendor/autoload.php';
 use GuzzleHttp\Client;
 use Sunra\PhpSimple\HtmlDomParser;
 $client = new Client();
-
-$res = $client->request('POST', 'https://biletebi.ge/startup.aspx?ajax=1&action=searchfortrain', [
-	'form_params' => [
-		'date' => '03/08/2018',
-		'from' => '56014',
-		'fromStation' => 'თბილისი-სამგზ',
-     		'oneComp' => 'false',
-		'to' => '57151',
-		'toStation' => 'ბათუმი',
-	]
-]);
-$body = $res->getBody();
-preg_match('/<b>[0-9]{2}:[0-9]{2}<\/b>.+/', $body , $matches, PREG_OFFSET_CAPTURE);
-//foreach($matches[0] as $match){
-//	echo $match;		
-//}
-//var_dump($matches);
-//echo $body;
-$html = HtmlDomParser::str_get_html($body);
-for($i = 0; $i < 15; $i++){
-	$div = $html->find('div[class=trainDates]', $i);
-
-	//var_dump($divs);
-	echo $div.'<br>';
-}
 $dt = new DateTime(date("Y-m-d"));
-echo $dt->modify('+1 day')->format('d/m/Y');
-//echo $date("d/m/Y")->modify('+1 day');
+$dt->modify('-1 day');
+$batumi = array('name' => 'ბათუმი', 'number' => '57151');
+$tbilisi = array('name' => 'თბილისი-სამგზ', 'number' => '56014');
+
+//$date -> string d/m/Y 
+function getResponse($from, $to, $date, $client){
+	$res = $client->request('POST', 'https://biletebi.ge/startup.aspx?ajax=1&action=searchfortrain', [
+		'form_params' => [
+			'date' => $date,
+			'from' => $from['number'],
+			'fromStation' => $from['name'],
+			'oneComp' => 'false',
+			'to' => $to['number'],
+			'toStation' => $to['name'],
+		]
+	]);
+
+	return $res->getBody();
+}
+
+for($i = 0; $i < 7; $i++){
+	$body = getResponse($tbilisi, $batumi, $dt->modify('+1 day')->format('d/m/Y'), $client);
+	preg_match('/<b>[0-9]{2}:[0-9]{2}<\/b>.+/', $body , $matches, PREG_OFFSET_CAPTURE);
+	$html = HtmlDomParser::str_get_html($body);
+
+	for($a = 0; $a < 15; $a++){
+		$div = $html->find('div[class=trainDates]', $a);
+		if($div === NULL){
+			break;
+		}
+		//var_dump($divs);
+		echo $div.'<br>';
+	}
+}
